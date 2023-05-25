2c2cfa25ee3d05a745cfc5a38e44a6a11bb2fd5d
Gubo Diba <gubodibawario@gmail.com>
2023-05-25 20:19:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+document.getElementById(`container`).style.backgroundColor=`silver`
+document.getElementById(`title`).style.color=`green`;
+
+
+let h3=document.getElementById("heading3")
+h3.textContent = h3.textContent.toUpperCase();
+
+let fru=document.createElement(`newfruit`)
+document.getElementById(`newfruit`).appendChild(fru)
+fru.innerHTML=`Apple`
+
+let veg=document.createElement(`newveg`)
+document.getElementById(`newveg`).appendChild(veg)
+veg.innerHTML=`cabbage`
